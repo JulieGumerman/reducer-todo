a6b43f32fff28fb6b55fb42e4862b499e8d13c90
bodhibrdr <julie.gumerman@gmail.com>
2019-09-10 16:56:41 -0600

    and it's an MVP

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -31,15 +31,13 @@ function App() {
       <div>
       {state.todos.map(todo => 
                 <div key={todo.id}>
-                    <h2>{todo.item}</h2>
+                    <h2 id={todo.item}>{todo.item}</h2>
                     <button onClick={event => {
                         event.preventDefault();
                         dispatch({ type: "TOGGLE_TODO", payload: todo.id})
-                        // if (todo.completed === true) {
-                        //   document.querySelector("h2").classList.add("is-true");
-                        // } else {
-                        //   document.querySelector.classList.remove("is-true");
-                        }
+                        const changeStyle = document.getElementById(`${todo.item}`);
+                        changeStyle.classList.toggle("is-done");
+                      }
                     }>Finished!!!</button>
                 </div>
             )}
